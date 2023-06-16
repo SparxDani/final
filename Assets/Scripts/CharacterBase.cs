c3d019929d918ccc88ccdb3163f257af8e51e191
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-06-16 10:03:00 -0500

    Salto mejorado

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -5,15 +5,17 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class CharacterBase : MonoBehaviour
- {
+{
     public Rigidbody2D rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
 
     private float horizontal;
+    private bool isJumping = false; // Variable para controlar si el personaje está saltando
+    private bool isFacingRight = true;
+
     public float speed = 8f;
     public float JumpingPower = 16f;
-    private bool isFacingRight = true;
 
     private void Awake()
     {
@@ -22,41 +24,53 @@ public class CharacterBase : MonoBehaviour
 
     private void Update()
     {
-        rb.velocity = new Vector2 (horizontal * speed, rb.velocity.y);
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
         if (!isFacingRight && horizontal > 0f)
         {
             Flip();
         }
-        else if (!isFacingRight && horizontal < 0f)
+        else if (isFacingRight && horizontal < 0f)
         {
             Flip();
         }
-        
     }
+
     public void Jump(InputAction.CallbackContext context)
     {
-        if (context.performed && IsGrounded())
+        if (context.performed && IsGrounded() && !isJumping)
         {
             rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
+            isJumping = true; // Marcamos que el personaje está saltando
         }
-        if (context.canceled && rb.velocity.y > 0f)
+        else if (context.canceled && rb.velocity.y > 0f)
         {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 2f);
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
     }
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position, 0f, groundLayer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Restablecer la capacidad de saltar cuando el personaje toca el suelo
+        if (collision.collider.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 
     private void Flip()
     {
-        isFacingRight = !isFacingRight; 
+        isFacingRight = !isFacingRight;
         Vector3 localScale = transform.localScale;
         localScale.x *= -1f;
         transform.localScale = localScale;
     }
+
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
