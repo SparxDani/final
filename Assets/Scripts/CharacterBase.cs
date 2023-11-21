dc4819b547e2a9d3327722e73124f2bd86681b80
SparxDani <86084707+SparxDani@users.noreply.github.com>
2023-11-21 11:03:14 -0500

    Movement Fixed

--- Assets/Scripts/CharacterBase.cs
+++ Assets/Scripts/CharacterBase.cs
@@ -1,58 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static CharacterBase;
 
 public class CharacterBase : MonoBehaviour, IPlayerController
 {
-    //Declaración de variables
-    public Rigidbody2D rb;
-    //public Transform groundCheck;
-    //public LayerMask groundLayer;
-
-    /*private float raycastLenght;
-    private bool isJumping = false;
-    private bool isFacingRight = true;
-    private int jumpsRemaining = 2;
-    //public InputManagerController manager;
-    public float JumpingPower = 9.5f;
-    public float horizontal;*/
-
-
-    private float time;
-    private CapsuleCollider2D capCol;
-
-    //Inputs
-    private Inputs _inputs;
-    private bool snapInput = true;
-    private Vector2 frameVelocity;
-    private Players controls;
-
-    //Movement
-    [SerializeField] private float maxSpeed = 8f;
-    [SerializeField] private float acceleration = 100;
+    [Header("Inputs Settings")]
+    [SerializeField] private LayerMask PlayerLayer;
+    [SerializeField] private bool snapInput = true; 
     [SerializeField] private float VdeadZoneLimit = 0.5f;
     [SerializeField] private float HdeadZoneLimit = 0.1f;
-    [SerializeField] private bool grounded;
+    
+    private Rigidbody2D rb;
+    private float time;
+    private CapsuleCollider2D capCol;
+    private Inputs _inputs;
+    private Vector2 frameVelocity;
+    private Players controls;
+    
+
+
+    [Header("Character Movement")]
+    [SerializeField] private float maxSpeed = 8f;
+    [SerializeField] private float acceleration = 100;
     [SerializeField] private float groundDeceleration = 50;
     [SerializeField] private float airDeceleration = 20;
+    [SerializeField] private float groundedForce = -2f;
+    [SerializeField] private float groundDistanceDetection = 0.10f;
+
+    private bool grounded;
     private bool queryColliders;
+    private float frameLeftGrounded = float.MinValue;
+
+
+    [Header("Character Jumping")]
+    [SerializeField] private float initialJumpForce = 32;
+    [SerializeField] private float fallingMaxSpeed;
+    [SerializeField] private float fallingAccel = 98.7f;
+    [SerializeField] private float endJumpEarlyModifier;
+    [SerializeField] private float CoyoteJump;
+    [SerializeField] private float BufferJump;
+
+    private bool coyoteUsable;
+    private float timeJumpWasPressed;
+    private bool jumpToConsume;
+    private bool bufferedJumpUsable;
+    private bool endedJumpEarly;
+
+
+    private bool HasBufferedJump => bufferedJumpUsable && time < timeJumpWasPressed + BufferJump;
+    private bool CanUseCoyote => coyoteUsable && !grounded && time < frameLeftGrounded + CoyoteJump;
 
     public Vector2 Inputs => _inputs.movement;
+    public event Action<bool, float> GroundedChanged;
+    public event Action Jumped;
+
 
     public void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        capCol = GetComponent<CapsuleCollider2D>();
+        queryColliders = Physics2D.queriesStartInColliders;
+
         controls = new Players();
         controls.Enable();
-        queryColliders = Physics2D.queriesStartInColliders;
     }
 
     private void Update()
     {
-        time = Time.deltaTime;
+        time += Time.deltaTime;
         ReadInput();
     }
     private void OnDisable()
@@ -63,8 +81,8 @@ public class CharacterBase : MonoBehaviour, IPlayerController
     {
         _inputs = new Inputs
         {
-            jumpDown = Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.C),
-            jumpHeld = Input.GetButton("Jump") || Input.GetKey(KeyCode.C),
+            jumpDown = controls.Ingame.Jump.triggered,
+            jumpHeld = controls.Ingame.Jump.ReadValue<float>() > 0.1f,
             movement = controls.Ingame.Movement.ReadValue<Vector2>()
         };
 
@@ -73,102 +91,121 @@ public class CharacterBase : MonoBehaviour, IPlayerController
             _inputs.movement.x = Mathf.Abs(_inputs.movement.x) < HdeadZoneLimit ? 0 : Mathf.Sign(_inputs.movement.x);
             _inputs.movement.y = Mathf.Abs(_inputs.movement.y) < VdeadZoneLimit ? 0 : Mathf.Sign(_inputs.movement.y);
         }
+        if (_inputs.jumpDown)
+        {
+            jumpToConsume = true;
+            timeJumpWasPressed = time;
+        }
 
     }
     public void FixedUpdate()
     {
+        Collisions();
+
+        HandleJump();
         Direction();
+        Gravity();
+
         Move();
+    }
+
+    private void Collisions()
+    {
+        Physics2D.queriesStartInColliders = false;
+        bool groundHit = Physics2D.CapsuleCast(capCol.bounds.center, capCol.size, capCol.direction, 0, Vector2.down, groundDistanceDetection, ~PlayerLayer);
+        bool ceilingHit = Physics2D.CapsuleCast(capCol.bounds.center, capCol.size, capCol.direction, 0, Vector2.up, groundDistanceDetection, ~PlayerLayer);
+
+        if(ceilingHit)
+        {
+            frameVelocity.y = Mathf.Min(0, frameVelocity.y);
+        }
+
+        if (!grounded && groundHit)
+        {
+            grounded = true;
+            coyoteUsable = true;
+            bufferedJumpUsable = true;
+            endedJumpEarly = false;                                                                 
+            GroundedChanged?.Invoke(true, Mathf.Abs(frameVelocity.y));
+
+
+        }
+        else if (grounded && !groundHit)
+        {
+            grounded = false;
+            frameLeftGrounded = time;
+            GroundedChanged?.Invoke(false, 0);
+        }
+
+        Physics2D.queriesStartInColliders = queryColliders;
+
+    }
+
+    
+
+
+    private void HandleJump()
+    {
+        if (!endedJumpEarly && !grounded && !_inputs.jumpHeld && rb.velocity.y > 0)
+        {
+            endedJumpEarly = true;
+        }
+
+        if (!jumpToConsume && !HasBufferedJump)
+        {
+            return;
+        }
+
+        if (grounded || CanUseCoyote)
+        {
+            ExecuteJump();
+        }
+
+        jumpToConsume = false;
+    }
+
+    private void ExecuteJump()
+    {
+        endedJumpEarly = false;
+        timeJumpWasPressed = 0;
+        bufferedJumpUsable = false;
+        coyoteUsable = false;
+        frameVelocity.y = initialJumpForce;
+        Jumped?.Invoke();
     }
     private void Direction()
     {
-        if(_inputs.movement.x == 0)
+        if (_inputs.movement.x == 0)
         {
             var decelerate = grounded ? groundDeceleration : airDeceleration;
-            frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, 0, Time.deltaTime);
+            frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, 0, decelerate * Time.fixedDeltaTime);
         }
         else
         {
             frameVelocity.x = Mathf.MoveTowards(frameVelocity.x, _inputs.movement.x * maxSpeed, acceleration * Time.fixedDeltaTime);
         }
     }
-
-    public void Move()
+    private void Gravity()
+    {
+        if (grounded && frameVelocity.y <= 0f)
+        {
+            frameVelocity.y = groundedForce;
+        }
+        else
+        {
+            var gravityInAir = fallingAccel;
+            if (endedJumpEarly && frameVelocity.y > 0)
+            {
+                gravityInAir *= endJumpEarlyModifier;
+            }
+            frameVelocity.y = Mathf.MoveTowards(frameVelocity.y, -fallingMaxSpeed, gravityInAir * Time.fixedDeltaTime);
+        }
+    }
+    private void Move()
     {
         rb.velocity = frameVelocity;
     }
-    /*private void OnEnable()
-    {
-        movementInput.Enable();
-        movementInput.Ingame.Movement.performed += Movement_performed;
-        movementInput.Ingame.Movement.canceled += Movement_canceled;
-    }
 
-    private void OnDisable()
-    {
-        movementInput.Disable();
-        movementInput.Ingame.Movement.performed -= Movement_performed;
-        movementInput.Ingame.Movement.canceled -= Movement_canceled;
-    }
-
-    private void Movement_canceled(InputAction.CallbackContext obj)
-    {
-        //movementMap = Vector2.zero;
-        movementMap = obj.ReadValue<Vector2>();
-        //horizontal = context.ReadValue<Vector2>().x;
-        if (movementMap != null ) { }
-    }
-
-    private void Movement_performed(InputAction.CallbackContext obj)
-    {
-        movementMap = obj.ReadValue<Vector2>();
-    }*/
-
-
-
-
-    /*public void Jump(InputAction.CallbackContext context)
-    {
-        if (context.performed && IsGrounded())
-        {
-            rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
-            isJumping = true;
-            jumpsRemaining = 1;
-        }
-        else if (context.performed && jumpsRemaining > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, JumpingPower);
-            isJumping = true;
-            jumpsRemaining--;
-        }
-        else if (context.canceled && rb.velocity.y > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
-        }
-    }
-
-    public bool IsGrounded()
-    {
-        return Physics2D.Raycast(groundCheck.position, Vector2.down, raycastLenght, groundLayer);
-    }
-
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Ground"))
-        {
-            isJumping = false;
-            jumpsRemaining = 2;
-        }
-    }
-
-    public void Flip()
-    {
-        isFacingRight = !isFacingRight;
-        Vector3 localScale = transform.localScale;
-        localScale.x *= -1f;
-        transform.localScale = localScale;
-    }
-    */
 }
 public struct Inputs
 {
@@ -179,4 +216,7 @@ public struct Inputs
 public interface IPlayerController
 {
     public Vector2 Inputs { get; }
+    public event Action<bool, float> GroundedChanged;
+    public event Action Jumped;
+
 }
